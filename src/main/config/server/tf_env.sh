8908f1ac1a01d0d85ccd22319ef7c7a7ca9e9b09
Mike Lynch <mike.lynch@iinet.net.au>
2015-08-17 14:17:23 +1000

    Manually set a bunch of config options in tf_env.sh (normally these would be populated by maven, I think.)

--- src/main/config/server/tf_env.sh
+++ src/main/config/server/tf_env.sh
@@ -3,22 +3,22 @@
 # this script sets the environment for other fascinator scripts
 #
 
-export SERVER_URL="${server.url.base}"
-export LOCAL_PORT="${server.port}"
-export PROJECT_HOME="${app.location.linux}"
-export AMQ_PORT="${amq.port}"
-export AMQ_STOMP_PORT="${amq.stomp.port}"
-export SMTP_HOST="${smtp.host}"
-export ADMIN_EMAIL="${admin.email}"
-export MINT_SERVER="${mint.proxy.server}"
-export MINT_AMQ="${mint.amq.broker}"
-export NON_PROXY_HOSTS="${non.proxy.hosts}"
+export SERVER_URL="http://redboxvm/"
+export LOCAL_PORT="9000"
+export PROJECT_HOME="/opt/redbox"
+export AMQ_PORT="9101"
+export AMQ_STOMP_PORT="9102"
+export SMTP_HOST="localhost"
+export ADMIN_EMAIL="admin@localhost"
+export MINT_SERVER="http://redboxvm:9001"
+export MINT_AMQ="http://redboxvm:9201"
+export NON_PROXY_HOSTS="localhost"
 
 # set fascinator home directory
 if [ -z "$TF_HOME" ]; then
 	export TF_HOME="$PROJECT_HOME/home"
 fi
-export REDBOX_VERSION="${redbox.version}"
+export REDBOX_VERSION="1.9-UTS"
 export FASCINATOR_HOME="$TF_HOME"
 
 # java class path
